8b978fcb9d61e0f7713984df139c6bff7cb58349
nansenho <nansen.ho@foxmail.com>
2021-05-24 08:30:15 +0800

    ..

--- index.ts
+++ index.ts
@@ -11,13 +11,16 @@ server.on('request', (request: IncomingMessage, response: ServerResponse) => {
     const {method, url: path, headers} = request;
     const {pathname, search} = url.parse(path);
 
-    const filename = pathname.substr(1); // 去掉 pathname 的 / 就得到了 filename
+    let filename = pathname.substr(1); // 去掉 pathname 的 / 就得到了 filename
+    if (filename === '') {
+        filename = 'index.html'
+    }
     fs.readFile(p.resolve(publicDir, filename), (error, data) => {
         if (error) {
-            // console.log(error);
-            if (error.errno === -4058) {
+            console.log(error);
+            if (error.errno === -4058 || error.errno === -2) {
                 response.statusCode = 404;
-                fs.readFile(p.resolve(publicDir, '404.html'), ()=>{
+                fs.readFile(p.resolve(publicDir, '404.html'), (error, data) => {
                     response.end(data);
                 })
             } else {
